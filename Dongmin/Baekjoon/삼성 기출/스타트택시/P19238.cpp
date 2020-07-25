1a7e12ce5987ba49aa640c6d7da5e1f9e2e53d22
dongmin lee <pkalsh345@gmail.com>
2020-07-25 19:31:05 +0900

    스타트택시 정답

--- Dongmin/Baekjoon/삼성 기출/스타트택시/P19238.cpp
+++ Dongmin/Baekjoon/삼성 기출/스타트택시/P19238.cpp
@@ -1,180 +1,176 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <list>
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
 using namespace std;
 typedef struct Pos {
     int row;
     int col;
 } Pos;
-typedef struct distPos {
-    Pos pos;
-    int distance;
-} DistPos;
-typedef struct PassengerInfo {
-    int num;
+typedef struct Passenger {
+    Pos start;
+    Pos dest;
+} Passenger;
+typedef struct DistPos {
     int dist;
-} PassengerInfo;
+    Pos pos;
+} DistPos;
 
-int N, M;
-int fuel;
-vector<vector<int> > grid;
+vector<vector<int> > map;
 vector<vector<bool> > visited;
-Pos taxiPos;
-vector<Pos> pStartPos;
-vector<Pos> pEndPos;
-list<int> remainedPassengers;
-int dr[] = {-1,0,1,0};
-int dc[] = {0,-1,0,1};
+Pos taxiStart;
+vector<Passenger> passengers;
+int N, M, fuel;
+Pos dir[4] = {{-1,0}, {0,1}, {1,0}, {0,-1}};
 
+bool operator==(const Pos& a, const Pos& b) {
+    return (a.row == b.row && a.col == b.col);
+}
+bool operator!=(const Pos& a, const Pos& b) {
+    return !(a==b);
+}
+Pos operator+(const Pos& a, const Pos& b) {
+    return Pos{a.row + b.row, a.col + b.col};
+}
 
 void getInput() {
     cin >> N >> M >> fuel;
-    grid.assign(N+2, vector<int>(N+2, -1));
-    for(int i=1; i<=N; i++) {
-        for(int j=1; j<=N; j++) {
-            cin >> grid[i][j];
+    map.assign(N+2, vector<int>(N+2, -1));
+    visited.assign(N+2, vector<bool>(N+2, false));
+    for(int i=1;i<=N;i++) {
+        for(int j=1;j<=N;j++) {
+            cin >> map[i][j];
         }
     }
-    cin >> taxiPos.row >> taxiPos.col;
-    pStartPos.assign(M, Pos{-1,-1});
-    pEndPos.assign(M, Pos{-1,-1});
-    for(int i=0; i<M; i++) {
-        cin >> pStartPos[i].row >> pStartPos[i].col >> pEndPos[i].row >> pEndPos[i].col;
-        remainedPassengers.push_back(i);
+
+    cin >> taxiStart.row >> taxiStart.col;
+    passengers.assign(M, Passenger({{-1,-1},{-1,-1}}));
+    for(int i=0;i<M;i++) {
+        cin >> passengers[i].start.row >> passengers[i].start.col >> passengers[i].dest.row >> passengers[i].dest.col;
     }
-    visited.assign(N+2, vector<bool>(N+2, false));
 }
-void initVisited() {
-    visited.assign(N+2, vector<bool>(N+2, false));
+
+int discoverPassenger(Pos pos) {
+    int ret = -1;
+    for(int i=0;i<passengers.size(); i++) {
+        if(passengers[i].start == pos && passengers[i].start != Pos{-1,-1}) {
+            ret = i;
+        }
+    }
+    return ret;
 }
-void visitCheck(Pos &pos) {
+
+void visitCheck(Pos& pos) {
     visited[pos.row][pos.col] = true;
 }
-int discoverPassenger(Pos pos) {
-    for(auto passenger:remainedPassengers) {
-        if(pStartPos[passenger].row == pos.row && pStartPos[passenger].col == pos.col) {
-            int ret = passenger;
-            return ret;
-        }
-    }
-    return -1;
-}
 
-PassengerInfo pickNextPassenger() {
-    int closestPassengerDist = 1000*N;
-    PassengerInfo closestPassenger = PassengerInfo{-1, -1};
-    int passRow = N+1, passCol = N+1;
-    int startPosCheck = discoverPassenger(taxiPos);
-    if(startPosCheck > 0) {
-        return PassengerInfo{startPosCheck, 0};
-    }
-
-   queue<DistPos> q;
-    q.push(DistPos{taxiPos,0});
-    visitCheck(taxiPos);
-    while(!q.empty()) {
-        DistPos here = q.front();
-        q.pop();
-        if(here.distance < closestPassengerDist) {
-            for(int i = 0; i < 4; i++) {
-                Pos next = Pos {here.pos.row + dr[i], here.pos.col + dc[i]};
-                if(!visited[next.row][next.col] && grid[next.row][next.col] == 0) {
-                    int passengerNum = discoverPassenger(next);
-                    if(passengerNum == -1) {
-                        q.push(DistPos{next, here.distance+1});
-                        visitCheck(next);
-                    }
-                    else {
-                        // 승객 스케줄링 정책
-                        if (closestPassengerDist >= here.distance+1) {
-                            closestPassengerDist = here.distance + 1;
-                            if(pStartPos[passengerNum].row < passRow) {
-                                closestPassenger = PassengerInfo{passengerNum, here.distance+1};
-                                passRow = pStartPos[passengerNum].row;
-                                passCol = pStartPos[passengerNum].col;
-                            }
-                            else if(pStartPos[passengerNum].row == passRow) {
-                                if(pStartPos[passengerNum].col < passCol) {
-                                    closestPassenger = PassengerInfo{passengerNum, here.distance+1};
-                                    passRow = pStartPos[passengerNum].row;
-                                    passCol = pStartPos[passengerNum].col;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    return closestPassenger;
-}
-
-bool isAvailableFuelSize(PassengerInfo info) {
-    if(info.dist > fuel) return false;
-    return true;
-}
-
-void moveToPassengerPos(PassengerInfo info) {
-    fuel -= info.dist;
-    taxiPos = pStartPos[info.num];
-}
-
-int moveToDestination(PassengerInfo info) {
+int pickNextPassenger() {
+    int next = -1;
+    int limitDist = 900000000;
     queue<DistPos> q;
-    q.push(DistPos{taxiPos,0});
-    visitCheck(taxiPos);
+    q.push({0, taxiStart});
+    visitCheck(taxiStart);
+
     while(!q.empty()) {
         DistPos here = q.front();
         q.pop();
-        for(int i = 0; i < 4; i++) {
-            Pos next = Pos {here.pos.row + dr[i], here.pos.col + dc[i]};
-            if(!visited[next.row][next.col] && grid[next.row][next.col] == 0) {
-                if(pEndPos[info.num].row == next.row && pEndPos[info.num].col == next.col){    
-                    taxiPos = pEndPos[info.num];
-                    fuel += (here.distance+1);
-                    //cout << "destination pos:[" << taxiPos.row << "," << taxiPos.col << "], remained fuel: " << fuel << ", distance: " << 
-                    // here.distance+1 << "\n";
-                    return 1;
-                }
+        if(here.dist > limitDist) {
+            break;
+        }
 
-                if(fuel > here.distance+1) {
-                    q.push(DistPos{next, here.distance+1});
-                    visitCheck(next);
+        int temp = discoverPassenger(here.pos);
+        if(temp > -1) {
+            limitDist = here.dist;
+            if(next == -1) {
+                next = temp;
+            }
+            else {
+                if(passengers[next].start.row > passengers[temp].start.row) {
+                    next = temp;
+                }
+                else if(passengers[next].start.row == passengers[temp].start.row) {
+                    if(passengers[next].start.col > passengers[temp].start.col) {
+                        next = temp;
+                    }
                 }
             }
         }
+        
+        for(int i=0;i<4;i++) {
+            Pos there = here.pos + dir[i];
+            if(!visited[there.row][there.col] && here.dist + 1 <= limitDist && map[there.row][there.col] == 0) {
+                q.push({here.dist + 1, there});
+                visitCheck(there);
+            }
+        }
     }
-    return -1;
+
+    if(limitDist >= fuel) {
+        next = -1;
+    }
+    fuel -= limitDist;
+    return next;
+}
+
+int goDestination(int passNum) {
+    int distance = -1;
+    queue<DistPos> q;
+    q.push({0, taxiStart});
+    visitCheck(taxiStart);
+
+    while(!q.empty()) {
+        DistPos here = q.front();
+        q.pop();
+        for(int i=0;i<4;i++) {
+            Pos there = here.pos + dir[i];
+            if(!visited[there.row][there.col] && map[there.row][there.col] == 0) {
+                q.push({here.dist + 1, there});
+                visitCheck(there);
+                if(passengers[passNum].dest == there) {
+                    distance = here.dist + 1;
+                    break;
+                } 
+            }
+        }
+    }
+
+    if(distance > fuel) {
+        distance = -1;
+    }
+    else {
+        fuel -= distance;
+    }
+    return distance;
+}
+
+void solution() {
+    for(int i=0;i<M;i++) { 
+        int nextPassenger = pickNextPassenger();
+        if(nextPassenger == -1) {
+            fuel = -1;
+            return;
+        }
+        //printf("go to #%d passenger, remained fuel: %d\n", nextPassenger, fuel);
+        taxiStart = passengers[nextPassenger].start;
+        visited.assign(N+2, vector<bool>(N+2, false));
+
+
+        int distance = goDestination(nextPassenger);
+        if(distance == -1) {
+            fuel = -1;
+            return;
+        }
+
+        taxiStart = passengers[nextPassenger].dest;
+        fuel += (2*distance);
+        //printf("go to #%d passenger dest, remained fuel: %d\n", nextPassenger, fuel);
+        passengers[nextPassenger].start = Pos{-1,-1};
+        visited.assign(N+2, vector<bool>(N+2, false));
+    }
 }
 
 int main() {
     getInput();
-    for(int i = 0; i < M; i++) {
-        // 태울 승객 선택
-        PassengerInfo nextPassenger = pickNextPassenger();
-        if(nextPassenger.num == -1) {
-            cout << "-1";
-            return 0;
-        }
-
-        //cout << "next passenger:[" << pStartPos[nextPassenger.num].row << "," << pStartPos[nextPassenger.num].col << "], distance: " << nextPassenger.dist << "\n";
-        remainedPassengers.remove(nextPassenger.num);
-        initVisited();
-        // 가능 여부 확인 -> 불가능하면 -1 출력하고 종료
-        if(!isAvailableFuelSize(nextPassenger)) {
-            cout << "-1";
-            return 0;
-        }
-        // 최단거리로 이동
-        moveToPassengerPos(nextPassenger);
-        // 목적지에 최단거리로 이동
-        int success = moveToDestination(nextPassenger);
-        if (success < 0) {
-            cout << "-1";
-            return 0;
-        }
-        initVisited();
-    }
-    cout << fuel;
+    solution();
+    printf("%d", fuel);
 }
