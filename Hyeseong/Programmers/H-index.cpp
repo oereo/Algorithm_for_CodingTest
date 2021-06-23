99c747d2cde86edeb1db7ace5be3824a01ee2cd0
hyeddo <55426397+hyeddo@users.noreply.github.com>
2021-06-23 12:34:34 +0900

    hyeseong

---
+++ Hyeseong/Programmers/H-index.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+	int answer = 0;
+
+	sort(citations.begin(), citations.end(), greater<int>());
+
+	if (!citations[0]) return 0;
+
+	for (int i = 0; i < citations.size(); i++) {
+		if (citations[i] > i) answer++;
+	}
+
+	return answer;
+}
